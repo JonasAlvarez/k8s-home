41768718f188d4070823b810f931d4852a3fa8e8
JonasAlvarez <jonas@sysadm.org>
2022-11-09 18:32:47 +0100

    Added Makefile automation to the Grafana logs Added comment in all Makefiles to show clearlly the deploy and uninstall progress

---
+++ logs/5-logs-daemonset.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "Error: no namespace"
+    exit 1
+fi
+NAMESPACE=$1
+
+MANIFEST_URL=https://raw.githubusercontent.com/grafana/agent/v0.27.1/production/kubernetes/agent-loki.yaml NAMESPACE=pro-logs /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/grafana/agent/v0.27.1/production/kubernetes/install-bare.sh)" | kubectl apply -n $NAMESPACE -f -
